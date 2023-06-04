137b7bd278ddd2d41aa60fe026e1a9e5b6c86a55
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-06-04 19:49:00 +0530

    Update Python Source Code.py

--- Language Models/BERT /Python Source Code.py
+++ Language Models/BERT /Python Source Code.py
@@ -1,82 +1,444 @@
-import os
-
+import re
+import random
 import numpy as np
-import pandas as pd
+import math
+import itertools
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+import tqdm
 
-from transformers import (
-    BertTokenizer , 
-    BertForSequenceClassification , 
-    Trainer , 
-    TrainingArguments)
+import transformers, datasets
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer
 
-from datasets import load_dataset
-
+from torch.utils.data import Dataset, DataLoader
 import torch
-from torch import nn
+import torch.nn.functional as F
+from torch.optim import Adam
 
-tokenizer = BertTokenizer.from_pretrained("/kaggle/input/huggingface-bert/bert-base-cased")
-model = BertForSequenceClassification.from_pretrained("/kaggle/input/huggingface-bert/bert-base-cased")
 
-model.classifier = nn.Linear(768 , 1)
-model.num_labels = 1
+tokenizer = BertWordPieceTokenizer(clean_text = True ,
+                                   handle_chinese_chars = False , 
+                                   strip_accents = False , 
+                                   lowercase = True)
+tokenizer.train( files = paths , vocab_size = 30_000 ,  min_frequency = 5 ,
+                limit_alphabet = 1000 , wordpieces_prefix = '##' ,
+                special_tokens=['[PAD]', '[CLS]', '[SEP]', '[MASK]', '[UNK]'])
 
-def tokenize_function(examples):
-    return tokenizer(examples['excerpt'], padding='max_length', truncation=True, max_length=512)
+os.mkdir('./bert-it-1')
+tokenizer.save_model('./bert-it-1', 'bert-it')
+tokenizer = BertTokenizer.from_pretrained('./bert-it-1/bert-it-vocab.txt', local_files_only=True)
 
-f_train_datasets = train_datasets.map(tokenize_function, batched=True)
-f_train_datasets = f_train_datasets.remove_columns(['id', 'url_legal', 'license', 'excerpt', 'standard_error'])
-f_train_datasets = f_train_datasets.rename_column('target', 'labels')
-f_train_datasets = f_train_datasets.shuffle(seed=42)
+class BERTDataset(Dataset):
+    def __init__(self, data_pair, tokenizer, seq_len=64):
 
-f_test_datasets = test_datasets.map(tokenize_function, batched=True)
-f_test_datasets = f_test_datasets.remove_columns(['url_legal', 'license', 'excerpt'])
+        self.tokenizer = tokenizer
+        self.seq_len = seq_len
+        self.corpus_lines = len(data_pair)
+        self.lines = data_pair
 
-n_samples = len(f_train_datasets['train'])
-n_train = int(0.9 * n_samples)
+    __len__ = lambda self : self.corpus_lines
 
-f_train_dataset = f_train_datasets['train'].select(range(n_train))
-f_eval_dataset = f_train_datasets['train'].select(range(n_train, n_samples))
+    get_random_line = lambda self : self.lines[random.randrange(len(self.lines))][1]
+    
+    def get_corpus(self, item) : return self.lines[item][0], self.lines[item][1]
 
-f_test_dataset = f_test_datasets['train']
+    def get_sent(self, index):
 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    logits, labels = logits.squeeze(), labels.squeeze()
-    rmse = np.sqrt(np.mean((labels - logits) ** 2))
-    return {'RMSE': rmse}
+        t1, t2 = self.get_corpus_line(index)
 
-training_args = TrainingArguments(
-    'training_args',
-    num_train_epochs = 5,
-    logging_steps = 200,
-    learning_rate = 1e-4,
-    per_device_train_batch_size = 8,
-    per_device_eval_batch_size = 8,
-    evaluation_strategy = 'steps'
-)
+        if random.random() > 0.5 : return t1 , t2 , 1
+        else : return t1, self.get_random_line() , 0
 
-trainer = Trainer(
-    model = model,
-    train_dataset = f_train_dataset,
-    eval_dataset = f_eval_dataset,
-    compute_metrics = compute_metrics,
-    args = training_args
-)
+    def random_word(self, sentence):
+        
+        tokens = sentence.split()
+        output_label = []
+        output = []
 
-trainer.train()
+        for i, token in enumerate(tokens):
+            
+            prob = random.random()
 
-trainer.evaluate()
+            token_id = self.tokenizer(token)['input_ids'][1:-1]
 
-pred_output = trainer.predict(f_test_dataset)
-pred_targets = pred_output.predictions.squeeze()
-pred_ids = f_test_dataset['id']
+            if prob < 0.15:
+                
+                prob /= 0.15
 
-submission = pd.DataFrame({
-    'id': pred_ids,
-    'target': pred_targets
-})
+                if prob < 0.8:
+                    
+                    for i in range(len(token_id)):
+                        
+                        output.append(self.tokenizer.vocab['[MASK]'])
 
-submission.to_csv('submission.csv', index=False)
+                elif prob < 0.9:
+                    
+                    for i in range(len(token_id)):
+                        
+                        output.append(random.randrange(len(self.tokenizer.vocab)))
+
+
+                else:
+                    
+                    output.append(token_id)
+
+                output_label.append(token_id)
+
+            else:
+                
+                output.append(token_id)
+                
+                for i in range(len(token_id)):
+                    
+                    output_label.append(0)
+
+        output = list(itertools.chain(*[[x] if not isinstance(x, list) else x for x in output]))
+        output_label = list(itertools.chain(*[[x] if not isinstance(x, list) else x for x in output_label]))
+        
+        assert len(output) == len(output_label)
+        
+        return output, output_label
+
+    def __getitem__(self, item):
+
+        t1, t2, is_next_label = self.get_sent(item)
+
+        t1_random, t1_label = self.random_word(t1)
+        t2_random, t2_label = self.random_word(t2)
+
+
+        t1 = [self.tokenizer.vocab['[CLS]']] + t1_random + [self.tokenizer.vocab['[SEP]']]
+        t1_label = [self.tokenizer.vocab['[PAD]']] + t1_label + [self.tokenizer.vocab['[PAD]']]
+        
+        t2 = t2_random + [self.tokenizer.vocab['[SEP]']]
+        t2_label = t2_label + [self.tokenizer.vocab['[PAD]']]
+
+        segment_label = (
+            [
+                1 for _ in range(len(t1))
+            ] + 
+            [
+                2 for _ in range(len(t2))
+            ]
+        )[:self.seq_len]
+        
+        bert_input = (t1 + t2)[:self.seq_len]
+        bert_label = (t1_label + t2_label)[:self.seq_len]
+        
+        padding = [
+            self.tokenizer.vocab['[PAD]'] 
+            for _ in range(self.seq_len - len(bert_input))
+        ]
+        
+        bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
+
+        output = {
+            "bert_input": bert_input,
+            "bert_label": bert_label,
+            "segment_label": segment_label,
+            "is_next": is_next_label
+        }
+
+        return {
+            key: torch.tensor(value) 
+            for key, value in output.items()
+        }
+
+class PositionalEmbedding(torch.nn.Module):
+
+    def __init__(self, d_model, max_len=128):
+        super().__init__()
+
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        for pos in range(max_len):   
+
+            for i in range(0, d_model, 2):   
+                
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+
+
+        self.pe = pe.unsqueeze(0)   
+
+
+    def forward(self, x): return self.pe
+    
+class BERTEmbedding(torch.nn.Module):
+
+    def __init__(self, vocab_size, embed_size, seq_len=64, dropout=0.1):
+
+        super().__init__()
+
+        self.embed_size = embed_size
+
+        self.token = torch.nn.Embedding(vocab_size, embed_size, padding_idx=0)
+        self.segment = torch.nn.Embedding(3, embed_size, padding_idx=0)
+
+        self.position = PositionalEmbedding(d_model=embed_size, max_len=seq_len)
+        self.dropout = torch.nn.Dropout(p=dropout)
+       
+    def forward(self, sequence, segment_label):
+
+        x = self.token(sequence) + self.position(sequence) + self.segment(segment_label)
+
+        return self.dropout(x)
+    
+class MultiHeadedAttention(torch.nn.Module):
+    
+    def __init__(self, heads, d_model, dropout=0.1):
+        
+        super(MultiHeadedAttention, self).__init__()
+        
+        assert d_model % heads == 0
+        
+        self.d_k = d_model // heads
+        
+        self.heads = heads
+        self.dropout = torch.nn.Dropout(dropout)
+
+        self.query = torch.nn.Linear(d_model, d_model)
+        self.key = torch.nn.Linear(d_model, d_model)
+        self.value = torch.nn.Linear(d_model, d_model)
+        
+        self.output_linear = torch.nn.Linear(d_model, d_model)
+        
+    def forward(self, query, key, value, mask):
+
+        query = self.query(query)
+        key = self.key(key)        
+        value = self.value(value)   
+
+        query = query.view(query.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)   
+        key = key.view(key.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)  
+        value = value.view(value.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)  
+
+        scores = torch.matmul(query, key.permute(0, 1, 3, 2)) / math.sqrt(query.size(-1))
+
+        scores = scores.masked_fill(mask == 0, -1e9)    
+
+        weights = F.softmax(scores, dim=-1)           
+        weights = self.dropout(weights)
+
+        context = torch.matmul(weights, value)
+
+        context = context.permute(0, 2, 1, 3).contiguous().view(context.shape[0], -1, self.heads * self.d_k)
+
+        return self.output_linear(context)
+    
+class FeedForward(torch.nn.Module):
+
+    def __init__(self, d_model, middle_dim=2048, dropout=0.1):
+        super(FeedForward, self).__init__()
+        
+        self.fc1 = torch.nn.Linear(d_model, middle_dim)
+        self.fc2 = torch.nn.Linear(middle_dim, d_model)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.activation = torch.nn.GELU()
+
+    def forward(self, x):
+        out = self.activation(self.fc1(x))
+        out = self.fc2(self.dropout(out))
+        return out
+
+class EncoderLayer(torch.nn.Module):
+    
+    def __init__(self , d_model=768 , heads=12 , 
+                 feed_forward_hidden=768 * 4 , dropout=0.1):
+        
+        super(EncoderLayer, self).__init__()
+        
+        self.layernorm = torch.nn.LayerNorm(d_model)
+        
+        self.self_multihead = MultiHeadedAttention(heads, d_model)
+        self.feed_forward = FeedForward(d_model, middle_dim=feed_forward_hidden)
+        
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, embeddings, mask):
+
+        interacted = self.dropout(self.self_multihead(embeddings, embeddings, embeddings, mask))
+
+        interacted = self.layernorm(interacted + embeddings)
+
+        feed_forward_out = self.dropout(self.feed_forward(interacted))
+        encoded = self.layernorm(feed_forward_out + interacted)
+        
+        return encoded
+    
+class BERT(torch.nn.Module):
+
+    def __init__(self, vocab_size, d_model=768, n_layers=12, heads=12, dropout=0.1):
+
+        super().__init__()
+
+        self.d_model = d_model
+        self.n_layers = n_layers
+        self.heads = heads
+
+        self.feed_forward_hidden = d_model * 4
+
+        self.embedding = BERTEmbedding(vocab_size=vocab_size, embed_size=d_model)
+
+        self.encoder_blocks = torch.nn.ModuleList(
+            [
+                EncoderLayer(d_model, heads, d_model * 4, dropout) 
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x, segment_info):
+        
+        mask = (x > 0).unsqueeze(1).repeat(1, x.size(1), 1).unsqueeze(1)
+
+        x = self.embedding(x, segment_info)
+
+        for encoder in self.encoder_blocks:
+            
+            x = encoder.forward(x, mask)
+        
+        return x
+    
+ class NextSentencePrediction(torch.nn.Module):
+
+    def __init__(self, hidden):
+
+        super().__init__()
+
+        self.linear = torch.nn.Linear(hidden, 2)
+        self.softmax = torch.nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+
+        return self.softmax(self.linear(x[:, 0]))
+    
+class MaskedLanguageModel(torch.nn.Module):
+
+    def __init__(self, hidden, vocab_size):
+
+        super().__init__()
+
+        self.linear = torch.nn.Linear(hidden, vocab_size)
+        self.softmax = torch.nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        
+        return self.softmax(self.linear(x))
+    
+ class BERTLM(torch.nn.Module):
+
+    def __init__(self, bert: BERT, vocab_size):
+
+        super().__init__()
+        
+        self.bert = bert
+        
+        self.next_sentence = NextSentencePrediction(self.bert.d_model)
+        self.mask_lm = MaskedLanguageModel(self.bert.d_model, vocab_size)
+
+    def forward(self, x, segment_label):
+        x = self.bert(x, segment_label)
+        
+        return self.next_sentence(x), self.mask_lm(x)
+    
+class ScheduledOptim():
+
+    def __init__(self, optimizer, d_model, n_warmup_steps):
+        self._optimizer = optimizer
+        self.n_warmup_steps = n_warmup_steps
+        self.n_current_steps = 0
+        self.init_lr = np.power(d_model, -0.5)
+
+    def step_and_update_lr(self):
+
+        self._update_learning_rate()
+        self._optimizer.step()
+
+    def zero_grad(self):
+
+        self._optimizer.zero_grad()
+
+    def _get_lr_scale(self):
+        return np.min([
+            np.power(self.n_current_steps, -0.5),
+            np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+
+    def _update_learning_rate(self):
+
+        self.n_current_steps += 1
+        lr = self.init_lr * self._get_lr_scale()
+
+        for param_group in self._optimizer.param_groups:
+            param_group['lr'] = lr
+            
+class BERTTrainer:
+
+    def __init__(self , model , train_dataloader , test_dataloader = None , 
+                 lr = 1e-4 , weight_decay = 0.01 , betas = (0.9 , 0.999) , 
+                 warmup_steps = 10000 , log_freq = 10 , device = 'cuda'):
+
+        self.device = device
+        self.model = model
+        
+        self.train_data = train_dataloader
+        self.test_data = test_dataloader
+
+        self.optim = Adam(self.model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay)
+        self.optim_schedule = ScheduledOptim(self.optim, self.model.bert.d_model, n_warmup_steps=warmup_steps)
+
+        self.criterion = torch.nn.NLLLoss(ignore_index=0)
+        self.log_freq = log_freq
+    
+    def train(self, epoch) : self.iteration(epoch, self.train_data)
+
+    def test(self, epoch) : self.iteration(epoch, self.test_data, train=False)
+
+    def iteration(self, epoch, data_loader, train=True):
+        
+        avg_loss = 0.0
+        total_correct = 0
+        total_element = 0
+        
+        mode = "train" if train else "test"
+
+        data_iter = tqdm.tqdm(enumerate(data_loader) , desc="EP_%s:%d" % (mode, epoch), 
+                              total=len(data_loader) , bar_format="{l_bar}{r_bar}")
+
+        for i, data in data_iter:
+
+            data = {key: value.to(self.device) for key, value in data.items()}
+
+            next_sent_output, mask_lm_output = self.model.forward(data["bert_input"], data["segment_label"])
+            next_loss = self.criterion(next_sent_output, data["is_next"])
+
+            mask_loss = self.criterion(mask_lm_output.transpose(1, 2), data["bert_label"])
+            loss = next_loss + mask_loss
+
+            if train:
+
+                self.optim_schedule.zero_grad()
+                loss.backward()
+                self.optim_schedule.step_and_update_lr()
+
+            correct = next_sent_output.argmax(dim=-1).eq(data["is_next"]).sum().item()
+            avg_loss += loss.item()
+
+            total_correct += correct
+            total_element += data["is_next"].nelement()
+            
+ train_data = BERTDataset(pairs, seq_len = 64, tokenizer=tokenizer)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True, pin_memory=True)
+
+bert_model = BERT(vocab_size=len(tokenizer.vocab) , 
+                  d_model=768 , n_layers=2 , 
+                  heads=12 , dropout=0.1)
+
+bert_lm = BERTLM(bert_model, len(tokenizer.vocab))
+bert_trainer = BERTTrainer(bert_lm, train_loader, device='cpu')
+epochs = 20
+
+for epoch in range(epochs):
+    bert_trainer.train(epoch)
+
